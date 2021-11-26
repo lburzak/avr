807187901b3d27a9f242156ea395b546e76027a3
Łukasz Burzak <lukasz.burzak@pollub.edu.pl>
2021-11-26 18:55:55 +0100

    Implement timers ad2

--- avr-app/main.c
+++ avr-app/main.c
@@ -2,56 +2,41 @@
 #define F_CPU 1000000L
 #include <util/delay.h>
 
-void delay_10ms();
-void delay_1s();
-void init_ad1();
-void run_ad1();
+void init_ad2();
+void run_ad2();
 
 int main() {
-	run_ad1();
+	run_ad2();
 	
 	return 0;
 }
 
-void init_ad1() {
+void init_ad2() {
 	DDRA = 0xFF;
 	PORTA = 0x00;
-	
-	// Ustawia prescaler w tryb 1/256
-	TCCR0 |= (1 << CS02);
-	
-	// Ustawia tryb CTC
-	TCCR0 |= (1 << WGM01);
-	
-	// Ustawia maksymalna wartosc licznika
-	OCR0 = 0.01 * F_CPU / 256 - 1;
-	
-	// Resetuje licznik
-	TCNT0 = 0;
+	DDRB = 0x00;
+	PORTB = 0xFF;
+	// Ustawia prescaler zbocza opadajacego, tryb NORMAL
+	TCCR0 |= (1 << CS02) | (1 << CS01);
+	// Ustawia prescaler zbocza opadajacego, tryb NORMAL
+	// TCCR0 |= (1 << CS02) | (1 << CS01) | (1 << CS00);
+	TCNT0 = 240;
 }
 
-void run_ad1() {
-	init_ad1();
+void run_ad2() {
+	init_ad2();
 	
-	while(1) {
-		// Opoznia iteracje o 1 sekunde
-		delay_1s();
-		// Przelacza LED
-		PORTA ^= (1 << PA0);
+	uint8_t counter;
+	while (1) {
+		_delay_ms(200);
+		// Odczytuje stan licznika
+		counter = TCNT0;
+		// Ustawia LEDy w taki sposób aby odzwierciedlaly liczbe nacisniec
+		PORTA = counter;
+		// Sprawdza stan flagi przepelnienia
+		if (TIFR & (1 << TOV0)) {
+			// Zatrzymuje timer
+			TCCR0 &= (0 << CS02) | (0 << CS01) | (0 << CS00);
+		}
 	}
-}
-
-void delay_10ms() {
-	while (!(TIFR & (1 << OCF0)));
-	
-	// Zdejmuje flage przepelnienia
-	TIFR |= (1 << OCF0);
-	
-	// Resetuje licznik
-	TCNT0 = 0;
-}
-
-void delay_1s() {
-	for (uint8_t i = 0; i < 100; i++)
-		delay_10ms();
 }
