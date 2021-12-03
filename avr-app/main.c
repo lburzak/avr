b14d27b8e801696e5e20bb6c5e34ad43b3d1b0fd
Łukasz Burzak <lukasz.burzak@pollub.edu.pl>
2021-12-03 19:13:20 +0100

    Interrupts: Ad 3

--- avr-app/main.c
+++ avr-app/main.c
@@ -1,52 +1,38 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#define F_CPU 1000000L
 
-/* Obsluguje przerwanie INT0 */
-ISR(INT0_vect) {
-	PORTA ^= 1 << PA0;
-}
+// Zmienna w SRAM, do przechowywania stanu przyciskow
+volatile uint8_t buttons = 0;
 
-/* Obsluguje przerwanie INT2 */
-ISR(INT2_vect) {
-	PORTA ^= 1 << PA2;
-}
-
-/* Przyjmuje wartosc MCUCR i zwraca z ustawionym
-wyzwalaniem INT0 na zboczach opadajaych */
-uint8_t INT0_falling(uint8_t bits) {
-	bits &= ~(1 << ISC00);
-	bits |= 1 << ISC01;
-	return bits;
-}
-
-/* Przyjmuje wartosc MCUCR i zwraca z ustawionym
-wyzwalaniem INT0 na zboczach narastajacych */
-uint8_t INT0_raising(uint8_t bits) {
-	return bits | 1 << ISC00 | 1 << ISC01;
+// Obsluguje przerwania wywolane przez Timer 0 w trybie CTC
+ISR(TIMER0_COMP_vect) {
+	// Przechowuje stan przyciskow (nieistotne bity sa odrzucone)
+	buttons = PINA & 0x0f;
 }
 
 int main(void) {
-	DDRA = 1 << PA0 | 1 << PA2;
+	DDRA = 0x00;
+	DDRC = 0xff;
 	
-	PORTB = 1 << PB2;
-	PORTD = 1 << PD2;
+	// Ustawia Timer 0 w tryb CTC
+	TCCR0 |= (1 << WGM01) | (0 << WGM00);
 	
-	cli();
+	// Ustawia preskaler 64
+	TCCR0 |= (1 << CS01) | (1 << CS00);
 	
-	// Ustawia wyzwalanie INT0 INT2 na zboczach narastajacych
-	MCUCR = INT0_raising(MCUCR);
-	MCUCSR |= 1 << ISC2;
+	// Ustawia liczbe impulsow, po ktorej nastepuje przerwanie// Przerwanie ma wystepowac po 0.005 s
+	OCR0 = F_CPU / 64 * 0.005 - 1;
 	
-	// Ustawia wyzwalanie INT0 INT2 na zboczach opadajacych
-	// MCUCR = INT0_falling(MCUCR);
-	// MCUCSR &= 0 << ISC2;
+	// Resetuje stan licznika
+	TCNT0 = 0;
 	
-	// Aktywuje przerwania INT0 INT2
-	GICR |= 1 << INT0 | 1 << INT2;
-	GIFR |= 1 << INT0 | 1 << INT2;
+	// Aktywuje przerwania Timera 0 w trybie CTC
+	TIMSK |= (1 << OCIE0);
 	
 	// Aktywuje obsluge przerwan
 	sei();
 	
-	while (1);
+	// Petla glowna wyswietla stan przyciskow na LEDach
+	while (1) PORTC = buttons;
 }
