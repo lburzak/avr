1a0083516d311655278052aa4e6618a8c101cd56
Łukasz Burzak <lukasz.burzak@pollub.edu.pl>
2021-12-08 23:30:45 +0100

    Add docs

--- avr-app/main.c
+++ avr-app/main.c
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include "keypad.h"
 #define F_CPU 1000000L
 
 // Zmienna w SRAM, do przechowywania stanu przyciskow
@@ -7,13 +8,25 @@ volatile uint8_t buttons = 0;
 
 // Obsluguje przerwania wywolane przez Timer 0 w trybie CTC
 ISR(TIMER0_COMP_vect) {
-	// Przechowuje stan przyciskow (nieistotne bity sa odrzucone)
-	buttons = PINA & 0x0f;
+	// Zapisuje kod wcisnietego przycisku do zmiennej globalnej
+	buttons = keypad_read();
 }
 
 int main(void) {
-	DDRA = 0x00;
-	DDRC = 0xff;
+	DDRA = 0xf0;
+	PORTA = 0xff;
+	
+	// LEDy zostaly podlaczone do portu B, zamiast C ze wzgledu na wade stykow plytki mikrokontrolera na zajeciach
+	DDRB = 0xff;
+	
+	// Ustawia port klawiatury
+	struct PortConfig keypad_config = {
+		.DDR = &DDRA,
+		.PIN = &PINA,
+		.PORT = &PORTA
+	};
+	
+	keypad_set_config(&keypad_config);
 	
 	// Ustawia Timer 0 w tryb CTC
 	TCCR0 |= (1 << WGM01) | (0 << WGM00);
@@ -21,7 +34,8 @@ int main(void) {
 	// Ustawia preskaler 64
 	TCCR0 |= (1 << CS01) | (1 << CS00);
 	
-	// Ustawia liczbe impulsow, po ktorej nastepuje przerwanie// Przerwanie ma wystepowac po 0.005 s
+	// Ustawia liczbe impulsow, po ktorej nastepuje przerwanie
+	// Przerwanie ma wystepowac po 0.005 s
 	OCR0 = F_CPU / 64 * 0.005 - 1;
 	
 	// Resetuje stan licznika
@@ -33,6 +47,6 @@ int main(void) {
 	// Aktywuje obsluge przerwan
 	sei();
 	
-	// Petla glowna wyswietla stan przyciskow na LEDach
-	while (1) PORTC = buttons;
+	// Petla glowna wyswietla kod przycisku na LEDach
+	while (1) PORTB = buttons;
 }
